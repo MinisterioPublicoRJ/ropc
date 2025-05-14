8398379730418af2d9ed680bea4387f5e6d7d9a1
geysson.melo <johndoe@example.com>
2025-05-14 12:14:40 -0300

    atualiza static

--- conf/settings.py
+++ conf/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 import dj_database_url
@@ -152,14 +152,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = config(
-    'STATIC_URL',
-    default='/static/'
-)
-STATIC_ROOT = config(
-    'STATIC_ROOT',
-    default="/static/"
-)
+# STATIC_URL = config(
+#     'STATIC_URL',
+#     default='/static/'
+# )
+# STATIC_ROOT = config(
+#     'STATIC_ROOT',
+#     default="/static/"
+# )
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     Path(BASE_DIR, "static"),
 ]
